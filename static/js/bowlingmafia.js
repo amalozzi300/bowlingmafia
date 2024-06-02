9fbb4a7a356c88cf9c3c94438033b3f40dd64f8f
amalozzi300 <andrewmalozzi900@gmail.com>
2024-06-01 23:15:51 -0400

    js to render uploaded  profile pic before save

---
+++ static/js/bowlingmafia.js
@@ -0,0 +1,16 @@
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $(".profile__image").attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(".edit-account__input").on('change', function() {
+    readURL(this);
+});
