c1de14d723c846773425bf17bd545a807bd051ae
amalozzi300 <andrewmalozzi900@gmail.com>
2024-06-03 15:47:02 -0400

    added new models to admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import BowlingCenter, League, Tournament
+from .models import (
+    BowlingCenter, 
+    League, 
+    LeagueSidepot,
+    LeagueRoster,
+    LeagueBowlerSidepotEntry,
+    LeagueBowlerEntry,
+    Tournament,
+)
 
 # Register your models here.
 admin.site.register(BowlingCenter)
 admin.site.register(League)
+admin.site.register(LeagueSidepot)
+admin.site.register(LeagueRoster)
+admin.site.register(LeagueBowlerSidepotEntry)
+admin.site.register(LeagueBowlerEntry)
 admin.site.register(Tournament)
--- sidepots/admin.py
+++ sidepots/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from .models import *
+
+from .models import (
+    HighGame,
+    HandicapHighGame,
+    HighSeries,
+    HandicapHighSeries,
+    WinnerTakeAll,
+    HandicapWinnerTakeAll,
+    Eliminator,
+    HandicapEliminator,
+    MysteryDoubles,
+    HandicapMysteryDoubles,
+)
 
 # Register your models here.
+admin.site.register(HighGame)
+admin.site.register(HandicapHighGame)
+admin.site.register(HighSeries)
+admin.site.register(HandicapHighSeries)
+admin.site.register(WinnerTakeAll)
+admin.site.register(HandicapWinnerTakeAll)
+admin.site.register(Eliminator)
+admin.site.register(HandicapEliminator)
+admin.site.register(MysteryDoubles)
+admin.site.register(HandicapMysteryDoubles)
+
