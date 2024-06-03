a6b8a10d28f7ed030a89a534930dcf32b94684be
amalozzi300 <andrewmalozzi900@gmail.com>
2024-06-03 15:34:19 -0400

    added sidepot models and roster models v1.0

--- events/models.py
+++ events/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
 import uuid
 from localflavor.us.models import USStateField, USZipCodeField
 from phonenumber_field.modelfields import PhoneNumberField
 
 from profiles.models import Profile
+from sidepots.models import Sidepot
 
 # Create your models here.
 class BowlingCenter(models.Model):
@@ -34,6 +38,44 @@ class Event(models.Model):
 
 class League(Event):
     admins = models.ManyToManyField(Profile, related_name='league_admin')
+    start_date = models.DateTimeField()
+
+class LeagueSidepot(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league_sidepots')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.UUIDField()
+    sidepot = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f'{self.league.name} -- {self.sidepot.name}'
+    
+class LeagueRoster(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league_rosters')
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.league.name} -- {self.date.strftime('%m/%d/%y')}'
+    
+class LeagueBowlerSidepotEntry(models.Model):
+    bowler_entry = models.ForeignKey('LeagueBowlerEntry', on_delete=models.CASCADE, related_name='league_sidepot_entries')
+    sidepot = GenericRelation(LeagueSidepot)
+    entry_count = models.PositiveIntegerField(default=1)
+
+    def clean(self):
+        if not self.sidepot.allow_multiple_entries and self.entry_count > 1:
+            raise ValidationError(f'Multiple entries are not allowed for {self.sidepot.name}')
+        
+    def __str__(self):
+        return f'{self.bowler_entry.bowler} -- {self.sidepot.name} x {self.entry_count}'
+    
+class LeagueBowlerEntry(models.Model):
+    league_roster = models.ForeignKey(LeagueRoster, on_delete=models.CASCADE, related_name='league_bowler_entries')
+    bowler = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    sidepots = models.ManyToManyField(LeagueSidepot, through=LeagueBowlerSidepotEntry, related_name='league_bowler_entries')
+
+    def __str__(self):
+        return f'{self.league_roster.league.name} -- {self.league_roster.date.strftime('%m/%d/%y')} -- {self.bowler}'
 
 class Tournament(Event):
     directors = models.ManyToManyField(Profile, related_name='tournament_director')
--- sidepots/models.py
+++ sidepots/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+import uuid
 
 # Create your models here.
+class Sidepot(models.Model):
+    name = models.CharField(max_length=64)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    entry_fee = models.DecimalField(max_digits=6, decimal_places=2)
+    payout_ratio = models.PositiveSmallIntegerField(default=6, validators=[MinValueValidator(2)])
+    allow_multiple_entries = models.BooleanField(default=False, editable=False)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return str(self.name)
+
+class HandicapSidepot(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    hdcp_percent = models.PositiveIntegerField(max_digits=3, validators=[MaxValueValidator(100)])
+    base_average = models.PositiveIntegerField(max_digits=3, validators=[MaxValueValidator(300)])
+
+    class Meta:
+        abstract = True
+
+class HighGame(Sidepot):
+    name = models.CharField(max_length=64, default='High Game Pot', editable=False)
+
+class HandicapHighGame(Sidepot, HandicapSidepot):
+    name = models.CharField(max_length=64, default='Handicap High Game Pot', editable=False)
+    
+class HighSeries(Sidepot):
+    name = models.CharField(max_length=64, default='High Series Pot', editable=False)
+
+class HandicapHighSeries(Sidepot, HandicapSidepot):
+    name = models.CharField(max_length=64, default='Handicap High Series Pot', editable=False)
+    
+class WinnerTakeAll(Sidepot):
+    name = models.CharField(max_length=64, default='Winner Take All Pot', editable=False)
+
+class HandicapWinnerTakeAll(Sidepot, HandicapSidepot):
+    name = models.CharField(max_length=64, default='Handicap Winner Take All Pot', editable=False)
+    
+class Eliminator(Sidepot):
+    name = models.CharField(max_length=64, default='Eliminator', editable=False)
+    reverse = models.BooleanField(default=False)
+
+    def __str__(self):
+        name = str(self.name)
+
+        if self.reverse:
+            name = f'Reverse {name}' 
+
+        return name
+
+class HandicapEliminator(Sidepot, HandicapSidepot):
+    name = models.CharField(max_length=64, default='Handicap Eliminator', editable=False)
+    reverse = models.BooleanField(default=False)
+
+    def __str__(self):
+        name = str(self.name)
+
+        if self.reverse:
+            name = f'Reverse {name}' 
+
+        return name
+
+class MysteryDoubles(Sidepot):
+    name = models.CharField(max_length=64, default='Mystery Doubles', editable=False)
+    allow_multiple_entries = models.BooleanField(default=True, editable=False)
+
+class HandicapMysteryDoubles(Sidepot, HandicapSidepot):
+    name = models.CharField(max_length=64, default='Handicap Mystery Doubles', editable=False)
+    allow_multiple_entries = models.BooleanField(default=True, editable=False)
+
+# BRACKETS
+
+# class Brackets(Sidepot):
+#     name = models.CharField(max_length=64, default='Brackets', editable=False)
+#     allow_multiple_entries = models.BooleanField(default=True, editable=False)
+#     reverse = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         name = str(self.name)
+
+#         if self.reverse:
+#             name = f'Reverse {name}' 
+
+#         return name
+
+# class HandicapBrackets(Sidepot, HandicapSidepot):
+#     name = models.CharField(max_length=64, default='Handicap Brackets', editable=False)
+#     allow_multiple_entries = models.BooleanField(default=True, editable=False)
+#     reverse = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         name = str(self.name)
+
+#         if self.reverse:
+#             name = f'Reverse {name}' 
+
+#         return name
